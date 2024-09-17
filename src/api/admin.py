5fa676e97e2df0b7a15711049ae3e1a65ef3b29e
hericlibong <hericlibong@gmail.com>
2024-09-17 04:41:34 +0200

    added shell tests & data & admin intreface

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Project, Contributor
 
-# Register your models here.
+# Inline for Contributor model
+class ContributorInline(admin.TabularInline):
+    model = Contributor
+    extra = 1  # Display 1 blank form by default
+
+# ProjectAdmin without the ManyToMany field directly in fieldsets
+class ProjectAdmin(admin.ModelAdmin):
+    # Define only the fields that are editable
+    fieldsets = [
+        (None, {'fields': ['title', 'description', 'type', 'author']}),
+    ]
+    inlines = [ContributorInline]
+    
+    # Exclude the 'created_time' field from the form as it is non-editable
+    exclude = ['created_time']
+
+admin.site.register(Project, ProjectAdmin)
+
+
+
+
+    
+
--- src/api/models.py
+++ src/api/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-def validate_role(value):
-
-    valid_roles = ['Developer', 'Tester', 'Manager', 'Reviewer']
-    if value not in valid_roles:
-        raise ValueError(f"{value} is not a valid role. Valid roles are {valid_roles}")
-        
 
 class Project(models.Model):
     TYPE_CHOICES = [
@@ -21,10 +15,10 @@ class Project(models.Model):
     title = models.CharField(max_length=100, help_text="Project title")
     description = models.TextField(help_text="Project description")
     type = models.CharField(max_length=20, choices=TYPE_CHOICES, help_text="Project type")
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, help_text="Project author") 
-    # Voir s'il est possible d'avoir le même related_name pour plusieurs champs 
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, help_text="Project author")  
     created_time = models.DateTimeField(auto_now_add=True, help_text="Project creation date")
-    contributors = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Contributor', related_name='projects', help_text="Project contributors")
+    contributors = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Contributor', 
+                                          related_name='projects', help_text="Project contributors")
 
     def __str__(self):
         return self.title
@@ -33,13 +27,11 @@ class Project(models.Model):
         # l'auteur du projet est automatiquement ajouté aux contributeurs
         super().save(*args, **kwargs)
         self.contributors.add(self.author)
-        self.save()
-
     
+
 class Contributor(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, help_text="User")
     project = models.ForeignKey(Project, on_delete=models.CASCADE, help_text="Project")
-    role = models.CharField(max_length=20, validators=[validate_role], help_text="User role in the project")
     
     class Meta:
         constraints = [
@@ -47,4 +39,4 @@ class Contributor(models.Model):
         ]
 
     def __str__(self):
-        return f"{self.user.username} - {self.project.title} - {self.role}"
+        return f"{self.user.username} - {self.project.title} "
