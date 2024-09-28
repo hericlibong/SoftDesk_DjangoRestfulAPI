1ddff3e2640439122afaea25713c401eb79edebb
hericlibong <hericlibong@gmail.com>
2024-09-29 01:03:10 +0200

    add rgpd & oswap to Userviews

--- src/accounts/permissions.py
+++ src/accounts/permissions.py
@@ -1,14 +1,21 @@
 from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import PermissionDenied
 
 
-class IsSelOrAdmin(BasePermission):
+class IsSelfOrAdmin(BasePermission):
     """
-    Permisssion qui permet à un utilisateur de modifier ou de supprimer son compte
+    Permisssion personnalisée qui permet à un utilisateur de modifier ou de supprimer son compte
     ou à un administrateur d'y accéder
     """
+
+    message = "Vous n'avez pas la permission d'accéder à ces informations."
+
     def has_object_permission(self, request, view, obj):
-        # Si l'utilisateur est un administrateur, il peut accéder à tous les comptes
+        # Si l'utilisateur est un administrateur, il a accès à tous les objets
         if request.user.is_staff:
             return True
-        # sinon il ne peut accéder qu'à ses propres informations
-        return obj == request.user
+        # Sinon, il ne peut accéder qu'à ses propres informations
+        if obj == request.user:
+            return True
+        # Lever une exception avec un message personnalisé
+        raise PermissionDenied(self.message)
